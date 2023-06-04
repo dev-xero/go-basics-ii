605bd883bb9540e41140e11aaa193599dc79fb1d
Xero kun <elo48858@gmail.com>
2023-06-04 16:43:50 +0100

    `FEAT` Implemented function to compute squares of floats

--- main.go
+++ main.go
@@ -6,11 +6,16 @@ import (
 	"strings"
 )
 
+func square(num float64) float64 {
+	return num * num
+}
+
 func main() {
 	var num float64 = math.Floor(3.14)
 	var someString string = "some string"
 	var titleString string = strings.ToUpper(someString)
 
 	fmt.Printf("The floor of 3.14 is %v\n", num)
+	fmt.Printf("The square of %v is %v\n", num, square(num))
 	fmt.Println(titleString)
 }
